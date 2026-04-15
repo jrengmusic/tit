29198065d997c58e523b518a4f5d7bc5704456fd
bayu <bayu@jrengmusic.com>
2026-04-15 10:19:50 +0700

    v0.0.2

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -4,6 +4,8 @@ set -e
 # Called by goreleaser after each build. Signs and notarizes macOS binaries.
 # Env vars set by goreleaser: HOOK_TARGET (e.g. darwin_arm64), HOOK_PATH (binary path)
 
+[[ "$(uname)" == "Darwin" ]] || exit 0
+
 case "$HOOK_TARGET" in
   darwin_*)
     echo "Signing $HOOK_PATH..."
